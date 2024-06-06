bef0ed39d4973318d6cc7790f7faa7a55f47a6ce
Hoja Arzanesh <ha1797@rit.edu>
2024-06-06 11:14:28 -0400

    added extensions UITableViewDelegate and UITableViewDataSource to ViewController class

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    extension ViewController: UITableViewDelegate {
+        
+    }
+    
+    extension ViewController: UITableViewDataSource {
+        
+    }
 
 
 }
