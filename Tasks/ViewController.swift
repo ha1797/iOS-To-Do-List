9a52518c3b975b723d3efe09d0155acaec69f2cb
Hoja Arzanesh <ha1797@rit.edu>
2024-06-06 11:42:50 -0400

    added tasks var to ViewController class, which will hold all the tasks that a user has entered

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
