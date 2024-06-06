8941b740523326adef617faa8a9f22572e13697a
Hoja Arzanesh <ha1797@rit.edu>
2024-06-06 11:18:33 -0400

    added method for delegate extension that handles taps on a cell

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -16,8 +16,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // communicates with UITableView to help it perform actions
     extension ViewController: UITableViewDelegate {
         
+        // handles taps on a cell
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
     
     extension ViewController: UITableViewDataSource {
