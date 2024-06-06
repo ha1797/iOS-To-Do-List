6ab65c8608e928550ab52055af84dd0fd91e83d2
Hoja Arzanesh <ha1797@rit.edu>
2024-06-06 13:35:05 -0400

    removed errors preventing build from succeeding, and changed implementation of cellForRowAt method to not use deprecated textLabel method

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -12,33 +12,34 @@ class ViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     var tasks = [String]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Get all current saved tasks
+    }
+}
+    
+// communicates with UITableView to help it perform actions
+extension ViewController: UITableViewDelegate {
+    
+    // handles taps on a cell
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
     
-    // communicates with UITableView to help it perform actions
-    extension ViewController: UITableViewDelegate {
-        
-        // handles taps on a cell
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            tableView.deselectRow(at: indexPath, animated: true)
-        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = tasks[indexPath.row]
+        cell.contentConfiguration = configuration
+        return cell
     }
-    
-    extension ViewController: UITableViewDataSource {
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return tasks.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: indexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-            cell.defaultContentConfiguration().text = tasks[indexPath.row]
-            return cell
-        }
-    }
-
-
 }
 
