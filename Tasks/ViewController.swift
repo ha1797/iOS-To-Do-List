26950798bbcdc812064b33ad2d6f112a121a7a7b
Hoja Arzanesh <ha1797@rit.edu>
2024-06-07 16:06:06 -0400

    implemented updateTasks method of ViewController

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -31,7 +31,17 @@ class ViewController: UIViewController {
     }
     
     func updateTasks() {
+        // tutorial used guard instead of if
+        // if cannot be used if contains else at end like this example
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
         
+        for x in 0..<count {
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String {
+                tasks.append(task)
+            }
+        }
     }
     
     @IBAction func didTapAdd() {
